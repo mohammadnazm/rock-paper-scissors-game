16af84dacb7d54175949a5793c90656ca23a33cb
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-15 19:59:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react"
+import Header from "./components/Header"
+import Play from "./components/Play"
+import Footer from "./components/Footer"
+import Game from "./components/Game"
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props
-    return (
-      <>
-        <h1>Hello {name}</h1>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
 }
 
 export default App
