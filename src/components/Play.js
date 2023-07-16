b5e5dcd3be8211784c8cd7018374964fbf95963a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-16 19:20:37 +0300

    add more futures

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Triangle } from "../images/bg-triangle.svg"
 
 const Play = ({ setMyChoice }) => {
   const setChoice = e => {
@@ -8,25 +9,28 @@ const Play = ({ setMyChoice }) => {
 
   return (
     <div className="play">
-      <Link to="/game">
-        <div data-id="paper" onClick={setChoice} className="icon icon-paper">
-          Paper
-        </div>
-      </Link>
-      <Link to="/game">
-        <div
-          data-id="scissors"
-          onClick={setChoice}
-          className="icon icon-scissors"
-        >
-          Scissors
-        </div>
-      </Link>
-      <Link to="/game">
-        <div data-id="rock" onClick={setChoice} className="icon icon-rock">
-          rock
-        </div>
-      </Link>
+      <img src={Triangle} alt="" className="triangle" />
+      <div className="items">
+        <Link to="/game">
+          <div data-id="paper" onClick={setChoice} className="icon icon-paper">
+            Paper
+          </div>
+        </Link>
+        <Link to="/game">
+          <div
+            data-id="scissors"
+            onClick={setChoice}
+            className="icon icon-scissors"
+          >
+            Scissors
+          </div>
+        </Link>
+        <Link to="/game">
+          <div data-id="rock" onClick={setChoice} className="icon icon-rock">
+            rock
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
