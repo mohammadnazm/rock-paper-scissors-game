51c3c9a7d303bfe0b49a478a937e8d0587940deb
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-17 20:54:08 +0300

    create modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,19 @@
 import React from "react"
 
 const Modal = () => {
-  return <div>Modal</div>
+  return ReactDOM.createPortal(
+    <div className="modal-container">
+      <div className="modal-box">
+        <div className="modal_header">
+          <h1>Rules</h1>
+          <button onClick={toggle}>
+            <img src="../images/icon-close.svg" alt="Close" srcset="" />
+          </button>
+        </div>
+        <img src="../images/image-rules.svg" alt="Rules" srcset="" />
+      </div>
+    </div>
+  )
 }
 
 export default Modal
