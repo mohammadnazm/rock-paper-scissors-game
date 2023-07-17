dc39cdfa1404140896c70ae4f8f951ce2a7defc6
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-17 19:06:28 +0300

    add more futures

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
-import React from "react"
-
+import React, { useState } from "react";
+import Modal from "./Modal";
 const Footer = () => {
-  return <div>Footer</div>
-}
+  const [modal, setModal] = useState(false);
 
-export default Footer
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/mohammadnazm" target="_blank">Mohammad Nazm</a>.
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
